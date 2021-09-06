31da5ddbce81273291118a3394aea6f12e3ddffe
北山裕真 <y16111032@gmail.com>
2021-09-06 09:52:58 +0900

    bootstrapとfontawesomeの追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import 'bootstrap';
+import '../stylesheets/application.scss';
+import '@fortawesome/fontawesome-free/js/all';
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
